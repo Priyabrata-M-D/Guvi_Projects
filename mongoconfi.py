96174b629e44c4c8c17c73ccf6bc1a4ffb27a5dc
PriyabrataDS <118111907+Priyabrata-M-D@users.noreply.github.com>
2023-02-20 16:16:35 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	mongoconfi.py

--- mongoconfi.py
+++ mongoconfi.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 import json
 
+
 # Define a function to upload data to MongoDB
 def upload_to_mongodb(data, mongodb_conn_str, db_name, collection_name):
     # Connect to MongoDB
-    mongodb_conn_str=()
+    mongodb_conn_str = ()
     client = MongoClient('mongodb://localhost:27017')
     db = client["twitter_scrapping"]
     collection = db["scrapped"]
