d9d9ed07c821d6c8f6ddb42630f3b3e819867c92
Priyabrata Moharana <priyabrata_moharana@yahoo.co.uk>
2023-03-19 17:40:34 +0530

    Revert "comitted"
    This reverts commit 92ae602dba8a5252cd28b819a48ab2b301b7240b.

--- tscrape.py
+++ tscrape.py
@@ -10,10 +10,10 @@ def scrape_twitter_data(hashtag, start_date, end_date, max_tweets):
         if i >= max_tweets:
             break
         tweets_list.append(
-            [tweet.date, tweet.id, tweet.url, tweet.user.username, tweet.replyCount, tweet.content, tweet.retweetCount,
+            [tweet.date, tweet.id, tweet.url, tweet.user.username, tweet.replyCount, tweet.retweetCount,
              tweet.lang, tweet.source, tweet.likeCount])
     tweets_df = pd.DataFrame(tweets_list,
-                             columns=["Date", "ID", "URL", "User", "Reply Count", "Content", "Retweet Count",
+                             columns=["Date", "ID", "URL", "User", "Reply Count", "Retweet Count",
                                       "Language",
                                       "Source", "Like Count"])
     return tweets_df
