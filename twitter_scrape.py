97eab155ef4f80c042160b54df2fa00ddc793cb3
Priyabrata Moharana <118111907+Priyabrata-M-D@users.noreply.github.com>
2023-05-07 01:44:44 +0530

    Update twitter_scrape.py

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -1,43 +1,49 @@
 import pandas as pd
 import pymongo
 import streamlit as st
-import time, datetime
+import time
+import datetime
 import snscrape.modules.twitter as sntwit
 
 # Creating connection to No sql database
-client=pymongo.MongoClient("mongodb://localhost:27017/")
-mydb=client["twitter_data"]
-tweets_df=pd.DataFrame()
-mini_df=pd.DataFrame()              
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = client["twitter_data"]
+tweets_df = pd.DataFrame()
+mini_df = pd.DataFrame()
 
 # Creating Streamlit dash board ---------1
 st.title('Twitter Data Scrapper')
-choices=st.selectbox("What you are looking for?", ['Keyword','Hashtag'])
-inputt=st.text_input("Enter keyword that you want to search",'Example: Bit Coin Market')
-start_date=st.date_input("Enter the Start date",datetime.date(2000,1,1),key='1')
-end_date=st.date_input("Enter the End date",datetime.date(2023,1,1),key='2')
-tweet_count=st.number_input("No. of tweets to scrape?", 0,1000,5)
+choices = st.selectbox("What you are looking for?", ['Keyword', 'Hashtag'])
+inputt = st.text_input(
+    "Enter keyword that you want to search", 'Example: Bit Coin Market')
+start_date = st.date_input("Enter the Start date",
+                           datetime.date(2000, 1, 1), key='1')
+end_date = st.date_input("Enter the End date",
+                         datetime.date(2023, 1, 1), key='2')
+tweet_count = st.number_input("No. of tweets to scrape?", 0, 1000, 5)
 
-#Scraping data using snscrape
-tweets=[]
+# Scraping data using snscrape
+tweets = []
 if inputt:
-  try:
-    if choices=='Keyword':
-      for a,twt in enumerate(sntwit.TwitterSearchScraper(f'{inputt} since:{start_date} until:{end_date}').get_items()):
-        if a>tweet_count:
-          break
-        tweets.append([ twt.date, twt.id, twt.user.username, twt.content, twt.lang, twt.likeCount, twt.replyCount,twt.retweetCount, twt.source, twt.url ])
-      tweets_df=pd.DataFrame(tweets,columns=["Date", "ID", "User","Content","Language","Like Count", "Reply Count", "Retweet Count", "Source", "URL", ])
-    else:
-      for a,twt in enumerate(sntwit.TwitterHashtagScraper(f'{inputt} since:{start_date} until:{end_date}').get_items()):
-        if a>tweet_count:
-          break
-        tweets.append([ twt.date, twt.id, twt.user.username, twt.content, twt.lang, twt.likeCount, twt.replyCount,twt.retweetCount, twt.source, twt.url ])
-      tweets_df=pd.DataFrame(tweets,columns=["Date", "ID", "User","Content","Language","Like Count", "Reply Count", "Retweet Count", "Source", "URL", ])
-  except sntwit.TwitterSearchScraper.TwitterSearchScraperError :
-    st.error('Error occurred while searching for tweets using keyword')
-  except sntwit.TwitterHashtagScraper.TwitterHashtagScraperError :
-     st.error('Error occurred while searching for tweets using hashtag')
+    try:
+        if choices == 'Keyword':
+            for a, twt in enumerate(sntwit.TwitterSearchScraper(f'{inputt} since:{start_date} until:{end_date}').get_items()):
+                if a > tweet_count:
+                    break
+                tweets.append([twt.date, twt.id, twt.user.username, twt.content, twt.lang,
+                              twt.likeCount, twt.replyCount, twt.retweetCount, twt.source, twt.url])
+            tweets_df = pd.DataFrame(tweets, columns=[
+                                     "Date", "ID", "User", "Content", "Language", "Like Count", "Reply Count", "Retweet Count", "Source", "URL", ])
+        else:
+            for a, twt in enumerate(sntwit.TwitterHashtagScraper(f'{inputt} since:{start_date} until:{end_date}').get_items()):
+                if a > tweet_count:
+                    break
+                tweets.append([ twt.date, twt.id, twt.user.username, twt.content, twt.lang, twt.likeCount, twt.replyCount,twt.retweetCount, twt.source, twt.url ])
+            tweets_df=pd.DataFrame(tweets,columns=["Date", "ID", "User","Content","Language","Like Count", "Reply Count", "Retweet Count", "Source", "URL", ])
+    except sntwit.TwitterSearchScraper.TwitterSearchScraperError :
+        st.error('Error occurred while searching for tweets using keyword')
+    except sntwit.TwitterHashtagScraper.TwitterHashtagScraperError :
+        st.error('Error occurred while searching for tweets using hashtag')
 else:
   st.warnings("Kindly make a Choice")
 
@@ -74,4 +80,4 @@ with st.sidebar:
 # DISPLAY THE DOCUMENTS IN THE SELECTED COLLECTION
 if not mini_df.empty: 
     st.write(f'({len(mini_df)} Records Found')
-    st.write(mini_df)
+    st.write(mini_df) 
